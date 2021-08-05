a3424d3de81b385b00400e3b344519231eaadc68
jaypatel31 <jaypatel32157@gmail.com>
2021-08-05 15:20:28 +0530

    route params express basic

---
+++ express-tutorials/5-basic-json.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const {products} = require('./data.js')
+
+app.get('/',(req,res)=>{
+    res.json(products)
+})
+
+
+
+app.listen(5000,()=>{
+    console.log(`Listening on port  5000`);
+})
