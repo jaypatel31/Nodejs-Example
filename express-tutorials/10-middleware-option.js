d7010cd82927c8b97a17936654157802b3562d2d
jaypatel31 <jaypatel32157@gmail.com>
2021-08-07 14:27:57 +0530

    HTTP Request in express

---
+++ express-tutorials/10-middleware-option.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan')
+const {logger} = require('./logger.js')
+const {authorize} = require('./authorize.js')
+
+// req => middleware => res
+
+// app.use('/api',logger)
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
+
+
+app.get('/',(req,res)=>{
+    res.send('Home')
+})
+
+app.get('/about',(req,res)=>{
+    res.send('About')
+})
+
+app.get('/api/products',(req,res)=>{
+    res.send('Products')
+})
+
+app.get('/api/items',(req,res)=>{
+    console.log(req.user)
+    res.send('Items')
+})
+
+app.listen(5000,()=>{
+    console.log(`Listening on port  5000`);
+})
