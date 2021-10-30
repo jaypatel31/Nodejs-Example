7d8b96f72900035814440987c8599a7f5e8722be
Jay Patel <jaypatel32157@gmail.com>
2021-10-30 10:47:18 +0530

    Filter implement

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -6,7 +6,7 @@ export const getAllProductsStatic = async (req,res)=>{
 }
 
 export const getAllProducts = async (req,res)=>{
-    const {featured,company,name} = req.query
+    const {featured,company,name,sort,fields} = req.query
     const queryObject = {}
 
     if(featured){
@@ -19,7 +19,24 @@ export const getAllProducts = async (req,res)=>{
         queryObject.name = {$regex:name,$options:'i'};
     }
     
-    const products = await Product.find(queryObject)
+    
+    let result = Product.find(queryObject)
+
+    if(sort){
+        result = result.sort(sort.replace(/,/g," ")) 
+    }
+    else{
+        result = result.sort('createdAt')
+    }
+    if(fields){
+        result = result.select(fields.replace(/,/g," "))
+    }
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+
+
+    const products = await result.limit(limit).skip((page-1)*limit);
 
     res.status(200).json({products,nbHits:products.length})
 }
