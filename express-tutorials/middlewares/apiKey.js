a9459895f65eff5ba464fad3795ba7ab79a69d9f
Jay Patel <jaypatel32157@gmail.com>
2022-02-13 16:46:37 +0530

    Api Key Example Added

--- express-tutorials/middlewares/apiKey.js
+++ express-tutorials/middlewares/apiKey.js
@@ -2,7 +2,7 @@ const ErrorHandler = require("../errors/ErrorHandler");
 
 const apiKey = (req,res,next) => {
     const api_key = "1234567";
-    const userApiKey = req.query.api_key
+    const userApiKey = req.query.api_key;
 
     if(userApiKey && userApiKey === api_key){
         next();
